67bc1df7f71b887b15ecadc852c0c72908b7978e
kaleb96 <yes505304@gmail.com>
2023-11-10 18:11:24 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# vue-charts
+ Practice Chart.js
---
+++ chart-lib/src/components/LineChart.vue
@@ -0,0 +1,49 @@
+<template>
+  <canvas id="lineChart"></canvas>
+</template>
+
+<script>
+
+import Chart from 'chart.js/auto';
+
+export default {
+    data() {
+        return {
+            chartData: {
+                labels: ['January', 'Febuary', 'March', 'April', 'May'],
+                datasets: [
+                    {
+                        label: 'My First Dataset',
+                        borderColor: 'rgb(75, 192, 192)',
+                        data: [65, 59, 80, 81, 56],
+                    },
+                ]
+            }
+        }
+    },
+
+    mounted() {
+        this.renderChart();
+    },
+    
+    methods: {
+        renderChart() {
+            const ctx = document.getElementById('lineChart');
+
+            new Chart(ctx, {
+                type : 'line',
+                data: this.chartData,
+                options: {
+                    scales: {
+                        y: { beginAtZero: true }
+                    }
+                }
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
